97f8c6675333147e965d5211067612acba1e3a53
Mehmet Arıkan <arikan.mehmett@gmail.com>
2018-05-11 01:23:40 +0300

    ödev hafta 7

---
+++ Odev_Hafta7.py
@@ -0,0 +1,79 @@
+a = [[1, 0], [0, 1]]
+b = [[5, 2], [1, 3]]
+
+def carpim(liste, sayi):
+    vektor=[]
+    gecici=[]
+    for i in liste:
+        for j in range(len(i)):
+            gecici.append((i[j]*sayi))
+        vektor.append(gecici)
+        gecici=[]
+    return vektor
+    
+def kartezyen_carpim(a,b):
+    matris=[]
+    gecici=[]
+    x=0
+    for i in a:
+        for j in b:
+            for k in range(len(j)):
+                gecici.append(i[x])
+                gecici.append(j[k])
+                matris.append(gecici)
+                gecici=[]
+            x=x+1
+        x=0
+    print(matris)
+    
+def dis_carpim(a,b):
+    matris=[]
+    gecici=[]
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            gecici.append(carpim(b,a[i][j]))
+        matris.append(gecici)
+        gecici = []
+    print(matris)
+    
+def ic_carpim(a,b):
+    result=[]
+    toplam=0
+    for i in range(len(a)):
+        for j in range(len(b[0])):
+            for k in range(len(b)):
+                toplam += a[i][k] * b[k][j]
+            result.append(toplam)
+            toplam=0
+    print(result)
+
+ic_carpim(a,b)
+#[5, 2, 1, 3]
+
+dis_carpim(a,b)
+#[[[[5, 2], [1, 3]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[5, 2], [1, 3]]]]
+
+kartezyen_carpim(a,b)
+#[[1, 5], [1, 2], [0, 1], [0, 3], [0, 5], [0, 2], [1, 1], [1, 3]]
+
+def determinant(matrix, mul):
+    width = len(matrix)
+    if width == 1:
+        return mul * matrix[0][0]
+    else:
+        sign = -1
+        total = 0
+        for i in range(width):
+            m = []
+            for j in range(1, width):
+                buff = []
+                for k in range(width):
+                    if k != i:
+                        buff.append(matrix[j][k])
+                m.append(buff)
+            sign *= -1
+            total += mul * solve(m, sign * matrix[0][i])
+        return total
+
+print(determinant(b, 1))
+#13
